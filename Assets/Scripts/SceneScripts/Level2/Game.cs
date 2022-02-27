586f2235476ce744d7077bd2e11a27340c4d0a39
Pengwyn-Requiem <31973059+Pengwyn-Requiem@users.noreply.github.com>
2022-02-27 00:43:50 -0500

    rearranged some files

--- Assets/Scripts/SceneScripts/Level2/Game.cs
+++ Assets/Scripts/SceneScripts/Level2/Game.cs
@@ -33,7 +33,7 @@ public class Game : LevelState
     // Update is called once per frame
     void Update()
     {
-        if (!isFailed)
+        if (!isFailed || !isWon)
         {
             if(trafficState > 0)
             {
